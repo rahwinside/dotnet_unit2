d695a4064dfaf0a10fb3134ba432f3a29bcd6c5c
Rahuls-iMac <rahwinside@gmail.com>
2020-09-09 09:26:07 +0530

    4

--- Unit2/Program4.cs
+++ Unit2/Program4.cs
@@ -5,7 +5,14 @@ namespace Unit2
     {
         public static void Main(string[] args)
         {
-
+            string s = Console.ReadLine();
+            var flip = 1;
+            for (int i = 0; i <= s.Length; i++) {
+                if (s[i] != s[s.Length - 1 - i])
+                    flip = 0;
+                    break;
+            }
+            Console.WriteLine(flip);
         }
     }
 }
