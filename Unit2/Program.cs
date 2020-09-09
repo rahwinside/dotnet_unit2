6e3de5cc9a7aaec44bd7c16a02bbe6511119d5db
Rahuls-iMac <rahwinside@gmail.com>
2020-09-09 09:09:22 +0530

    1

---
+++ Unit2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Unit2
+{
+    class MainClass
+    {
+        public static void MainX(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            string str3 = str1 + str2;
+            Console.WriteLine(str3);
+        }
+    }
+}
---
+++ Unit2/Program2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Unit2
+{
+    public class Program2
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
