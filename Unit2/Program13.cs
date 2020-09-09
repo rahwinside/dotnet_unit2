60680e52f5e99cbcc4cabe8843e3e4907729a89e
Rahuls-iMac <rahwinside@gmail.com>
2020-09-09 09:43:20 +0530

    13

---
+++ Unit2/Program13.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace Unit2
+{
+    public class Program13
+    {
+        public static void MainX(string[] args)
+        {
+            StringBuilder str = new StringBuilder(Console.ReadLine());
+            int prod = 1;
+            for (int i = 0; i < str.Length; i++)
+            {
+                prod *= (int)str[i];
+            }
+            Console.WriteLine(prod);
+        }
+    }
+}
