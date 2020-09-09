c05e6824eff90356e6497a0ca325698dd126353a
Rahuls-iMac <rahwinside@gmail.com>
2020-09-09 09:38:54 +0530

    10, 11

--- Unit2/Program10.cs
+++ Unit2/Program10.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Text;
+
 namespace Unit2
 {
     public class Program10
     {
-        public static void Main(string[] args)
+        public static void MainX(string[] args)
         {
+            StringBuilder str = new StringBuilder(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                sum += (int)str[i];
+            }
+            Console.WriteLine(sum);
         }
     }
 }
---
+++ Unit2/Program11.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Unit2
+{
+    class Program11
+    {
+        public static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            string str3 = str1 + str2;
+            Console.WriteLine(str3);
+        }
+    }
+}
