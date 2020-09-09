6425dd4117badc380ea9891de7b484c92df0f0f1
Rahuls-iMac <rahwinside@gmail.com>
2020-09-09 09:22:16 +0530

    3

--- Unit2/Program3.cs
+++ Unit2/Program3.cs
@@ -1,11 +1,30 @@
 ﻿using System;
+using System.Text;
+
 namespace Unit2
 {
-    public static void Main(string[] args)
-    {
-        public Program3()
+    public class Program3 {
+        public static void MainX(string[] args)
         {
 
+            StringBuilder str = new StringBuilder(Console.ReadLine());
+            char old = Console.ReadLine()[0];
+            char newC = Console.ReadLine()[0];
+
+            int count = 0;
+            try
+            {
+                while (str[count] != null)
+                {
+                    if (str[count] == old)
+                        str[count] = newC;
+                    count++;
+                }
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                Console.WriteLine(str);
+            }
         }
     }
 }
---
+++ Unit2/Program4.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Unit2
+{
+    public class Program4
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
