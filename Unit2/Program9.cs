0d8cd4d2171b73c2762965aac7631cc5bdd1dec6
Rahuls-iMac <rahwinside@gmail.com>
2020-09-09 09:46:57 +0530

    9

---
+++ Unit2/Program9.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace Unit2
+{
+    public class Program9
+    {
+        public static void Main(string[] args)
+        {
+            StringBuilder str = new StringBuilder(Console.ReadLine());
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (((int)str[i] >= 65 && (int)str[i] <= 90) || ((int)str[i] >= 97 && (int)str[i] <= 122))
+                    Console.Write(str[i]);
+            }
+        }
+    }
+}
